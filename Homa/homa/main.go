35e2879d7a672692bde2bd0fc4f09890a486dfa4
Parham Alvani <parham.alvani@gmail.com>
2017-01-14 06:00:14 +0330

    Main http file of homa was added.

---
+++ Homa/homa/main.go
@@ -0,0 +1,26 @@
+/*
+* +===============================================
+* | Author:        Parham Alvani (parham.alvani@gmail.com)
+* |
+* | Creation Date: 14-01-2017
+* |
+* | File Name:     main.go
+* +===============================================
+ */
+
+package homa
+
+import (
+	"log"
+	"net/http"
+)
+
+func Main() {
+	fs := http.FileServer(http.Dir("homa-ui/dist"))
+	http.Handle("/", fs)
+
+	log.Println("Listening...")
+	if err := http.ListenAndServe(":1373", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
