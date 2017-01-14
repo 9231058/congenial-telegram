364ca0c0b71140d22685f3644c2a14dd6b5d5014
Parham Alvani <parham.alvani@gmail.com>
2017-01-14 06:10:27 +0330

    Userinterface template was added.

---
+++ Homa/homa-ui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homaUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the homaUiApp
+ */
+angular.module('homaUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Homa/homa-ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homaUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the homaUiApp
+ */
+angular.module('homaUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
