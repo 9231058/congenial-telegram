6a65944896d4f07c326a7284ff9ebce2327f81a0
Parham Alvani <parham.alvani@gmail.com>
2017-01-14 06:00:31 +0330

    Homa serve was added.

---
+++ Homa/homa-serve.go
@@ -0,0 +1,17 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 14-01-2017
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import "./homa"
+
+func main() {
+	homa.Main()
+}
