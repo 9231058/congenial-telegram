1a308a9e793af843f6b7997a95ad880ad7521ea2
Parham Alvani <parham.alvani@gmail.com>
2017-01-15 11:03:55 +0330

    Write tags uid on serial with better performance.

--- Arduino/src/main.ino
+++ Arduino/src/main.ino
@@ -28,13 +28,10 @@ void loop() {
 	}
 
 	//Show UID on serial monitor
-	Serial.print("UID tag :");
 	String content= "";
 	for (byte i = 0; i < mfrc522.uid.size; i++) {
-		Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-		Serial.print(mfrc522.uid.uidByte[i], HEX);
 		content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
 		content.concat(String(mfrc522.uid.uidByte[i], HEX));
 	}
-	Serial.print("\n");
+	Serial.println("@" + content);
 }
