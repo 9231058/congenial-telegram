86b277169817bb71a47621928835974293ded22f
Parham Alvani <parham.alvani@gmail.com>
2017-01-15 09:44:50 +0330

    Tag UID reading was added.

--- Arduino/src/main.ino
+++ Arduino/src/main.ino
@@ -27,6 +27,14 @@ void loop() {
 		return;
 	}
 
-	/* Dump debug info about the card. PICC_HaltA() is automatically called. */
-	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+	//Show UID on serial monitor
+	Serial.print("UID tag :");
+	String content= "";
+	for (byte i = 0; i < mfrc522.uid.size; i++) {
+		Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+		Serial.print(mfrc522.uid.uidByte[i], HEX);
+		content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+		content.concat(String(mfrc522.uid.uidByte[i], HEX));
+	}
+	Serial.print("\n");
 }
